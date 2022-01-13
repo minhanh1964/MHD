fa1b763abd2da375c1cd681b7581a80a5f883c7c
dustin-erdner <dustin.erdner@web.de>
2022-01-13 19:37:23 +0100

    changing background and make buttons visible

--- pages/customer/landing-page/index.js
+++ pages/customer/landing-page/index.js
@@ -6,15 +6,17 @@ const { Title } = Typography;
 
 export default function LandingPage() {
   return (
-    <div className="container pt-5">
-      <Title level={1} className="text-gold">MHD Restaurant</Title>
-      <div className="pt-1">
-        <img src='/logo1.png' />
+    <div className="background-container">
+      <div className="container pt-5">
+        <Title level={1} className="text-gold">MHD Restaurant</Title>
+        <div className="pt-1">
+          <img src='/logo1.png' />
+        </div>
+        <Title level={2} className="pt-1 pb-1 text-gold">Make reservation, make life!</Title>
+        <Link href='/customer/select-date'>
+          <Button type="primary" className={style.antdButton}>Begin now</Button>
+        </Link>
       </div>
-      <Title level={2} className="text-gold pt-1 pb-1">Make reservation, make life!</Title>
-      <Link href='/customer/select-date'>
-        <Button type="primary" className={style.antdButton}>Begin now</Button>
-      </Link>
     </div>
   )
 }
--- pages/customer/select-date/index.js
+++ pages/customer/select-date/index.js
@@ -10,33 +10,43 @@ function onChange(date, dateString) {
 const { Title } = Typography;
 
 export default function SelectDate() {
-  return (
-    <div className='container pt-5'>
-      <div className={style.select}>
-        <Title level={1} className="text-white">Select Date</Title>
+  return (    
+    <div className="background-container">
+      <div className='container pt-5'>
+        <div className={style.select}>
+          <Title level={1} className="text-white">Select Date</Title>
+        </div>
+        <div className='pt-2'>
+          <Space direction="horizontal">
+            <DatePicker onChange={onChange} />
+            <TimePicker onChange={onChange} />
+          </Space>
+        </div>
+        <div className='pt-2 text-white'>
+          <Link href='/customer/choose-table'>            
+          <Button>
+            Choose Table (*optional)
+          </Button>
+          </Link></div>
+        <div className='pt-1 text-white'>
+          <Link href='/customer/choose-dish'>      
+            <Button>
+              Choose Dish (*optional)
+            </Button>    
+          </Link></div>
+        <Row className='pt-2' justify='center'>
+          <Col span={8}>
+            <Link href='/'>
+              <Button>Back</Button>
+            </Link>
+          </Col>
+          <Col span={8}>
+            <Link href='/customer/customer-info'>
+              <Button type="primary">Continue</Button>
+            </Link>
+          </Col>
+        </Row>
       </div>
-      <div className='pt-2'>
-        <Space direction="horizontal">
-          <DatePicker onChange={onChange} />
-          <TimePicker onChange={onChange} />
-        </Space>
-      </div>
-      <div className='pt-2 text-white'>
-        <Link href='/customer/choose-table'>Choose Table (*optional)</Link></div>
-      <div className='pt-1 text-white'>
-        <Link href='/customer/choose-dish'>Choose Dish (*optional)</Link></div>
-      <Row className='pt-2' justify='center'>
-        <Col span={8}>
-          <Link href='/'>
-            <Button>Back</Button>
-          </Link>
-        </Col>
-        <Col span={8}>
-          <Link href='/customer/customer-info'>
-            <Button type="primary">Continue</Button>
-          </Link>
-        </Col>
-      </Row>
     </div>
   )
 }
