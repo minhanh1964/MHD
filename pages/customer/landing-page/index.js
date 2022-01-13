e6937edf9533a80aac6b72b26b54fd9c143f52a0
minh.nguyen <minh.nguyen@srabmit.sra.uni-hannover.de>
2022-01-13 12:55:40 +0100

    landing-page improvement, update ulility and global css

--- pages/customer/landing-page/index.js
+++ pages/customer/landing-page/index.js
@@ -1,10 +1,20 @@
 import Link from 'next/link'
+import { Button, Typography } from 'antd'
+import style from './LandingPage.module.css';
+
+const { Title } = Typography;
 
 export default function LandingPage() {
-    return (
-      <div>
-          <p>MHD Restaurant</p>
-          <Link href='/customer/select-date'>Table Reservation</Link>
+  return (
+    <div className="container pt-5">
+      <Title level={1} className="text-gold">MHD Restaurant</Title>
+      <div className="pt-1">
+        <img src='/logo1.png' />
       </div>
-    )
-  }
+      <Title level={2} className="text-gold pt-1 pb-1">Make reservation, make life!</Title>
+      <Link href='/customer/select-date'>
+        <Button type="primary" className={style.antdButton}>Begin now</Button>
+      </Link>
+    </div>
+  )
+}
