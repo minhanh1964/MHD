24c7ffacc55f1726be10c49349b481d3d20ec676
dustin-erdner <dustin.erdner@web.de>
2022-01-13 21:34:27 +0100

    Customer Infor, Confirmation, Thank you added

--- pages/customer/confirmation/index.js
+++ pages/customer/confirmation/index.js
@@ -3,23 +3,27 @@ import { Button, Typography, Row, Col } from 'antd';
 
 const { Title } = Typography;
 
-export default function ThankYou() {
+export default function Confirmation() {
   return (    
     <div className="background-container">
       <div className='container pt-5'>        
-        <Title level={1} className="text-white">Thank You</Title>
+        <Title level={1} className="text-white">Confirmation</Title>
         <div className="pt-2 mx-auto w-50">
           <h3 className="text-white">
-            Thank you, your Reservation was successfull.<br/>
-            You will get a confirmation e-mail soon.<br /><br />
+            Can you confirm your data?<br /><br />
             Name: Max Mustermann<br />
             E-Mail: max.mustermann@abc.de<br/>
             Phone: 012345678 <br />
           </h3>
           <Row className='pt-2' justify='center'>
             <Col span={8}>
-              <Link href='/'>
-                <Button>Back to Start</Button>
+              <Link href='/customer/customer-info'>
+                <Button>Back</Button>
+              </Link>
+            </Col>
+            <Col span={8}>
+              <Link href='/customer/thank-you'>
+                <Button type="primary">Continue</Button>
               </Link>
             </Col>
           </Row>
--- pages/customer/customer-info/index.js
+++ pages/customer/customer-info/index.js
@@ -1,7 +1,37 @@
+import Link from 'next/link'
+import { Button, Typography, Form, Input, Row, Col } from 'antd';
+
+const { Title } = Typography;
+
 export default function CustomerInfo() {
-    return (
-      <div>
-          <p>Customer Info</p>
+  return (    
+    <div className="background-container">
+      <div className='container pt-5'>        
+        <Title level={1} className="text-white">Customer Information</Title>
+        <div className="pt-2 mx-auto text-white w-50">
+          <Form.Item label={<label style={{ color: "white" }}>Name</label>} name="name" rules={[{message: 'Please input your name!'},]}>
+            <Input />
+          </Form.Item>
+          <Form.Item label={<label style={{ color: "white" }}>E-Mail</label>} name="e-mail" rules={[{message: 'Please input your e-mail!'},]}>
+            <Input />
+          </Form.Item>
+          <Form.Item label={<label style={{ color: "white" }}>Phone</label>} name="phone" rules={[{message: 'Please input your phone!'},]}>
+            <Input />
+          </Form.Item>
+          <Row className='pt-2' justify='center'>
+            <Col span={8}>
+              <Link href='/customer/select-date'>
+                <Button>Back</Button>
+              </Link>
+            </Col>
+            <Col span={8}>
+              <Link href='/customer/confirmation'>
+                <Button type="primary">Continue</Button>
+              </Link>
+            </Col>
+          </Row>
+        </div>
       </div>
-    )
+    </div>
+  )
 }
