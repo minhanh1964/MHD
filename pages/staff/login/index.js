26515ac9ea6a4eebb02be2713e64327ae75314fa
minh.nguyen <minh.nguyen@srabmit.sra.uni-hannover.de>
2022-01-27 13:35:35 +0100

    Login site for staffs & Notice on not yet implemented sites

--- pages/staff/login/index.js
+++ pages/staff/login/index.js
@@ -1,7 +1,34 @@
+import Link from 'next/link'
+import { Button, Input, Row, Col, Typography, Space } from 'antd'
+import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons'
+
+const { Title } = Typography;
+
 export default function Login() {
-    return (
-      <div>
-          <p>Login</p>
+  return (
+    <div className="background-container">
+      <div className="container pt-5">
+        <Title level={1} className="text-gold">MHD Restaurant</Title>
+        <div className="pt-1">
+          <img src='/logo1.png' className="br-50" />
+        </div>
+        <div className="pt-5 mx-auto text-white w-50">
+          <Space direction="vertical">
+            <Input placeholder="input username" />
+            <Input.Password
+              placeholder="input password"
+              iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+            />
+          </Space>
+          <Row className='pt-2' justify='center'>
+            <Col span={24}>
+              <Link href='/staff/customer-list' passHref>
+                <Button type="primary">Login</Button>
+              </Link>
+            </Col>
+          </Row>
+        </div>
       </div>
-    )
+    </div>
+  )
 }
